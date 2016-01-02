c4b9853961e7a7495fd103f57ba50df555e0d942
Toni Guasch <antoni.guasch@uvic.cat>
2016-01-02 21:09:48 +0100

    Exercise 9.1 Perception systens: Eigen Robot coordinates

---
+++ build/CMakeFiles/eigen_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/eigen_exercise.dir/src/eigen_exercise.cpp.o"
+  "eigen_exercise.pdb"
+  "eigen_exercise"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/eigen_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/eigenexercise.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/eigenexercise.dir/src/eigenexercise.cpp.o"
+  "eigenexercise.pdb"
+  "eigenexercise"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/eigenexercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
